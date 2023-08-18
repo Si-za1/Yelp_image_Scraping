bf3b225dd98b6cc6335510b1e6c16c84e106e38c
Siza <laurenadhikari16@gmail.com>
2023-08-18 08:58:53 +0545

    updated the code to get the desired pair output

--- scraping.py
+++ scraping.py
@@ -3,24 +3,21 @@ import requests
 import csv
 import re
 
-
 url_name = ["outside", "drink", "inside", "menu", "food", "interior"]
 
 tab_urls = {
-        names: f"https://www.yelp.com/biz_photos/benemon-new-york?tab={names}"
-        for names in url_name            
-    }
+    names: f"https://www.yelp.com/biz_photos/benemon-new-york?tab={names}"
+    for names in url_name
+}
 
 def scrape_yelp_image(names):
-   
     if names not in tab_urls:
-        print("Url not found!")
+        print("URL not found!")
         return []
 
     url = tab_urls[names]
-    print("url is", url)
+    print("URL:", url)
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    print(response)
     doc = BeautifulSoup(response.text, "html.parser")
 
     image_links = []
@@ -30,39 +27,23 @@ def scrape_yelp_image(names):
         srcset = img.get("srcset")
         if srcset:
             urls = re.findall(r"https?://[^\s]+", srcset)
-            image_links.append(urls[0])
+            image_links.extend([(names, url) for url in urls])  # Pairing the tab name with each URL to get the desired output 
     
     return image_links
 
 if __name__ == '__main__':
-    scraped_data = {}
+    scraped_data = []
     
     for names in url_name:
         image_links = scrape_yelp_image(names)
-        scraped_data[names] = image_links
-
-if __name__ == '__main__':
-    scraped_data = {}
-    
-    for names in url_name:
-        image_links = scrape_yelp_image(names)
-        scraped_data[names] = image_links
+        scraped_data.extend(image_links)  # Extend the list with tab-name-url pairs to get the desired output
 
     # Storing the data in a CSV file
     csv_filename = "yelp_images.csv"
     with open(csv_filename, mode="w", newline="", encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Tab Name", "Image Links"])
-        for names, links in scraped_data.items():
-            csv_writer.writerow([names, "\n".join(links)])
+        csv_writer.writerow(["Tab Name", "Image Link"])  # Header row
+        for tab_name, image_link in scraped_data:
+            csv_writer.writerow([tab_name, image_link])  
 
     print("Data has been saved to", csv_filename)
-
-    
-if __name__ == '__main__':
-   
-   for names in url_name:
-        movies = scrape_yelp_image(names)
-        print(f"URL in {names} links:")
-        
-        print("\n")
